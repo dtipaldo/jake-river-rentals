53e6db65c0ef9cdb3d46c6ac0d50bea7bd409930
Dan Tipaldo <dtipaldo@gmail.com>
2019-09-14 18:19:35 -0400

    WIP: continue seperating frontend and backend

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,24 +1,25 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    timestamp: {
-        type: Date,
-        default: Date.now()
-    },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
-// eslint-disable-next-line no-undef
-module.exports = User = mongoose.model("users", UserSchema);
+// eslint-disable-next-line
+module.exports = User = mongoose.model('users', UserSchema);
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,32 +1,29 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const passport = require("passport");
-const users = require("./routes/api/users");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const users = require('./routes/api/users');
 
-const port = process.env.PORT || 5000 ;
+const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const db = require("../../config/keys.js").mongoURI;
-mongoose.connect(db, {useNewUrlParser: true})
+const db = require('../../config/keys.js').mongoURI;
+
+mongoose.connect(db, { useNewUrlParser: true })
 // eslint-disable-next-line no-console
-    .then(() => console.log("MongoDB connected"))
-    // eslint-disable-next-line no-console
-    .catch(e => console.log(e));
+  .then(() => console.log('MongoDB connected'))
+// eslint-disable-next-line no-console
+  .catch((e) => console.log("** Don't forget to whitelist this IP **", e));
 
 // Passport middleware
 app.use(passport.initialize());
-require("../../config/passport")(passport);
+require('./passport')(passport);
 // Routes
-app.use("/api/users", users);
+app.use('/api/users', users);
 
 
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Server up on port ${port}!`));
-
-
-
-
