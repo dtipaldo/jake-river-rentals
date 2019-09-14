1a3abdcec3df9dc9208eae99f9f6e463b9c037d7
Dan Tipaldo <dtipaldo@gmail.com>
2019-09-13 22:14:17 -0400

    MEN-Stack-users

--- src/server.js
+++ src/server.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const db = require("../db.js").mongoURI;
+const passport = require("passport");
+const users = require("./routes/api/users");
 
 const port = process.env.PORT || 5000 ;
 const app = express();
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+const db = require("../config/keys.js").mongoURI;
 mongoose.connect(db, {useNewUrlParser: true})
 // eslint-disable-next-line no-console
     .then(() => console.log("MongoDB connected"))
     // eslint-disable-next-line no-console
     .catch(e => console.log(e));
+
+// Passport middleware
+app.use(passport.initialize());
+require("../config/passport")(passport);
+// Routes
+app.use("/api/users", users);
+
 
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Server up on port ${port}!`));
