0b90ba44609d17e7a93d11960f723d244efeb6a3
Dan Tipaldo <dtipaldo@gmail.com>
2019-09-22 19:49:56 -0400

    WIP: Sign In Page

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import Bookmarks from '@material-ui/icons/Bookmarks';
 import Navbar from './navbar';
+import SignIn from "./SignIn";
 
 class App extends Component {
     render() {
@@ -10,6 +11,7 @@ class App extends Component {
                 <Navbar />
                 <p>Paddle the Jake River</p>
             </div>
+
         );
     }
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router} from 'react-router-dom'
 import App from "./app";
+import SignIn from "./SignIn";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <div>
+            <Route path="/" component={App}></Route>
+            <Route path="/login" component={SignIn}></Route>
+        </div>
+    </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
